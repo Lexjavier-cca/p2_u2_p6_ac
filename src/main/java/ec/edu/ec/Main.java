45f2e4f8badec36bfc613bbd4de65f75a69b9b3d
Alex Caiza <caizaa07@gmail.com>
2026-05-13 19:36:57 -0500

    Taller 13: Conexion a Base de datos

---
+++ src/main/java/ec/edu/ec/Main.java
@@ -0,0 +1,27 @@
+package ec.edu.ec;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class Main {
+    public static void main(String[] args) {
+        Quarkus.run(App.class,args);
+           
+       
+    }
+    public static class App implements QuarkusApplication{
+        
+        @Override
+        public int run(String... args) throws Exception {
+            System.out.println("Conexion a una base de datos");
+            
+            
+            return 0;
+
+            
+        }
+    }
+
+}

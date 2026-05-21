53eaf171dddb0f8bf69e891ca2d0d52c05faddb4
Alex Caiza <caizaa07@gmail.com>
2026-05-20 19:39:05 -0500

    Alex Caiza Taller 15

---
+++ src/main/java/ec/edu/uce/application/service/EstudianteService.java
@@ -0,0 +1,24 @@
+package ec.edu.uce.application.service;
+
+import ec.edu.uce.domain.model.Estudiante;
+import ec.edu.uce.domain.repositoy.EstudianteRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+public class EstudianteService {
+    @Inject
+    private EstudianteRepository estudianteRepository;
+    public void guardar(Estudiante estudiante){
+        this.estudianteRepository.crear(estudiante);
+    }
+    public Estudiante buscarPorId(Integer id){
+        return this.estudianteRepository.seleccionarPorId(id);
+    }
+    public void actualizar(Estudiante estudiante){
+        this.estudianteRepository.actualizar(estudiante);
+    }
+    public void eliminar(Integer id){
+        this.estudianteRepository.eliminar(id);
+    }
+}
---
+++ src/main/java/ec/edu/uce/infraestrucure/repository/EstudianteRepositoryImpl.java
@@ -0,0 +1,38 @@
+package ec.edu.uce.infraestrucure.repository;
+
+import javax.swing.text.html.parser.Entity;
+
+import ec.edu.uce.domain.model.Estudiante;
+import ec.edu.uce.domain.repositoy.EstudianteRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+@ApplicationScoped
+@Transactional
+public class EstudianteRepositoryImpl implements EstudianteRepository{
+    @Inject
+    private EntityManager em;
+
+    @Override
+    public void crear(Estudiante estudiante) {
+      this.em.persist(estudiante);
+    }
+
+    @Override
+    public Estudiante seleccionarPorId(Integer id) {
+       return this.em.find(Estudiante.class, id);
+    }
+
+    @Override
+    public void actualizar(Estudiante estudiante) {
+        this.em.merge(estudiante);
+    }
+
+    @Override
+    public void eliminar(Integer id) {
+        this.em.remove(this.seleccionarPorId(id));
+        
+    }
+
+}
